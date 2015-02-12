b90dd2d9e48e3e7565bcd2a8ce3f75420b0ca5de
Beck-Sisyphus <pshfls@gmail.com>
2015-02-12 15:43:33 -0800

    build the basic environment setting

---
+++ IOS/UltimateUp/UltimateUp/empty.m
@@ -0,0 +1,9 @@
+//
+//  empty.m
+//  UltimateUp
+//
+//  Created by Beck Pang on 2/12/15.
+//  Copyright (c) 2015 University of Washington, Seattle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
