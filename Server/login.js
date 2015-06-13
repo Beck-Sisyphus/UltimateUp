40d3c0aefd85c692ffbe314038c4602a1bdc8a1e
xkxx <xukexiang@gmail.com>
2015-06-12 21:02:00 -0700

    login done

--- Server/login.js
+++ Server/login.js
@@ -1,12 +1,15 @@
 var fetch = require('request-promise');
 
-var conf, r;
+var conf, r, db, users;
 
 var handle;
 
 module.exports = function(config, rethink) {
   conf = config;
   r = rethink;
+  db = r.db('ut');
+  users = db.table('users');
+  fb_tokens = db.table('fb_tokens');
   return handle;
 };
 
@@ -18,12 +21,14 @@ handle = function(soc) {
      console.info('cb', cb);
      cb(req);
   });
-  soc.on("login", function(req, cb) {
+  soc.on("fb_login", function(req, cb) {
     var fb_token = req.fb_token || null;
     var user_id = req.user_id || null;
+    var fb_res;
 
     if (!fb_token) {
-      return cb({"status": false});
+      cb({"status": false});
+      return;
     }
 
     fetch({
@@ -39,10 +44,50 @@ handle = function(soc) {
       if (!res.access_token) {
         throw "No access_token on successful login";
       }
-      // TODO: send to db
+      console.info(res);
+      fb_res = res;
+
+      // check if user exists
+      return users.getAll(fb_res.id, {index: 'facebook_id'}).toArray();
+    }).then(function(res) {
+      if (res.length === 0) {
+        // no such user - create one
+        return users.insert({
+          facebook_id: fb_res.id,
+          }).run().then(function(res) {
+            if (res.errors) {
+              throw "Failed to insert new user";
+            }
+            return res.generated_keys[0];
+          });
+      }
+      else if (res.id != user_id) {
+        // error
+        throw "id mismatch: " +  res.id + " " + user_id;
+      } else {
+        return res.id;
+      }
+    }).then(function (id) {
+      user_id = id;
+      // update token
+      return fb_tokens.insert({
+        id: id,
+        token: fb_res.access_token,
+        expiration: fb_res.expires_in
+      }, {conflict: 'replace'}).run();
+    }).then(function(res) {
+      if (res.errors) {
+        throw "Failed to update fb token";
+      }
+
+      cb({
+        status: true,
+        user_id: user_id,
+        access_token: 'TODO'
+      });
     }).catch(function(err) {
-      console.error(err);
-      return cb({"status": false});
+      console.error("[fb_login] error:", err);
+      cb({"status": false});
     });
   });
 
---
+++ Server/spec/spec.js
@@ -0,0 +1,62 @@
+var io = require('socket.io-client');
+var testData = require("./test-data");
+var socketURL = "http://127.0.0.1/mobi";
+var options = {
+  transports: ['websocket'],
+  'force new connection': true
+};
+
+
+var socketConnect = function(cb) {
+  var client = io.connect(socketURL, options);
+
+  client.on('connect', function() {
+    cb(client);
+  });
+};
+
+describe("Connection", function() {
+  it("should be able to handle new connections", function(done) {
+    socketConnect(function(client) {
+      client.disconnect();
+      done();
+    });
+  });
+});
+
+describe("Login", function() {
+
+  it("should be able to login via fb", function() {
+    socketConnect(function(client) {
+      client.emit("fb_login", {
+        fb_token: testData.fb_token
+      }, function(res) {
+        expect(res.status).toBeTruthy();
+        console.info(res);
+        client.disconnect();
+        done();
+      });
+    });
+  });
+
+  // it("should be able to login via token", function() {
+  //   socketConnect(function(client) {
+  //     client.emit("fb_login", {
+  //       fb_token: testData.fb_token
+  //     }, function(res) {
+  //       client.disconnect();
+  //       socketConnect(function(client2) {
+  //         client.emit("login", {
+  //           access_token: res.access_token,
+  //           user_id: res.user_id
+  //         }, function(res2) {
+  //           expect(res2.status).toBeTruthy();
+  //           expect(res2.user_id).toBe(res.user_id);
+  //           client.disconnect();
+  //           done();
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
+});
